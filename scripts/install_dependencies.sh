ea5d0d3a4f67294f90ea76b50f6c67edaa595bf3
LeoPhat <49784590+LeoPhat@users.noreply.github.com>
2020-06-28 19:41:55 +0700

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -2,3 +2,6 @@
 sudo yum install -y gcc-c++ make
 curl -sL https://rpm.nodesource.com/setup_12.x | sudo -E bash -
 sudo yum install -y nodejs
+git clone https://github.com/LeoPhat/basicnodejs.git
+cd basicnodejs
+npm install
