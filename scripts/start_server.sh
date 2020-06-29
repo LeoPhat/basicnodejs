cc108adee35e23a5fb23d01f5cac17d27fb7764c
LeoPhat <49784590+LeoPhat@users.noreply.github.com>
2020-06-29 15:20:52 +0700

    add commands

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,6 +2,8 @@
 cd /var/api/basicnodejs
 npm install
 sudo npm install pm2@latest -g
+pm2 kill
+rm -rf ~/.pm2
 pm2 start index.js --name "phat" -i 0
 echo $(pm2 status) > /home/ec2-user/output_start_server.txt
 
