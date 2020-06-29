43dc984725e1feb27c9353488756c0ed69e71733
LeoPhat <49784590+LeoPhat@users.noreply.github.com>
2020-06-29 14:51:38 +0700

    swap

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd /var/api/basicnodejs
 npm install
-echo $(pm2 status) > /home/ec2-user/output_start_server.txt
+
 pm2 start index.js --name "phat"
+echo $(pm2 status) > /home/ec2-user/output_start_server.txt
 
